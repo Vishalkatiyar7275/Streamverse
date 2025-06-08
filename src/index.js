811f199982c826b1f579dbfd970cf4a382a04459
Vishal Katiyar <vk2899547@gmail.com>
2025-06-08 16:39:49 +0530

    Connected to MongoDB Atlas

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+import express from "express";
+const app = express();
+import connectDB from "./db/index.js";
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+//Approach 1: Using async/await with try/catch
+
+// //Optimized approach to connect to MongoDB using IIFE
+// //Semicolon is necessary to avoid issues with minification or concatenation(cleaning purpose)
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//         app.on("error", (error) => {
+//             console.error("Error connecting to MongoDB: ", error);
+//             throw error;
+//         });
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+//         console.error("Error  connecting to MongoDb : ", error);
+//         throw error;
+//     }
+// })();  
+
+
+
+// //Correct approach to connect to MongoDB
+// // function connectToDatabase() {}
+// // connectToDatabase()
